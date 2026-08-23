3fe3e080b9591cacc853f5fe1b7cd6a7b4375cdd
trevorpowell54 <trevorpowell54@gmail.com>
2026-08-23 12:19:56 -0400

    Add files via upload

---
+++ GameAuthorizer.java
@@ -0,0 +1,13 @@
+package com.gamingroom.gameauth.auth;
+
+import io.dropwizard.auth.Authorizer;
+
+public class GameAuthorizer implements Authorizer<GameUser> 
+{
+    @Override
+    public boolean authorize(GameUser user, String role) {
+    	
+    	return user.getRoles().contains(role);
+    	
+    }
+}
